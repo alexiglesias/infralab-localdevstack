13a1d609b3db26a9dd8a5ef82adf92fa3c38ff3a
Alex Iglesias Reguant <alex.iglesias7@gmail.com>
2026-03-16 16:35:23 +0100

    simplify user table

--- db_backup.sql
+++ db_backup.sql
@@ -34,33 +34,25 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `userEmail` varchar(255) DEFAULT NULL,
-  `profileImg` varchar(255) DEFAULT NULL,
-  `profileImgPath` varchar(255) DEFAULT NULL,
   `dateOfBirth` varchar(255) DEFAULT NULL,
   `fatherName` varchar(255) DEFAULT NULL,
   `motherName` varchar(255) DEFAULT NULL,
   `gender` varchar(255) DEFAULT NULL,
   `maritalStatus` varchar(255) DEFAULT NULL,
   `permanentAddress` varchar(255) DEFAULT NULL,
-  `tempAddress` varchar(255) DEFAULT NULL,
-  `primaryOccupation` varchar(255) DEFAULT NULL,
-  `secondaryOccupation` varchar(255) DEFAULT NULL,
-  `skills` varchar(255) DEFAULT NULL,
-  `phoneNumber` varchar(255) DEFAULT NULL,
-  `secondaryPhoneNumber` varchar(255) DEFAULT NULL,
   `nationality` varchar(255) DEFAULT NULL,
   `language` varchar(255) DEFAULT NULL,
-  `workingExperience` varchar(255) DEFAULT NULL,
+  `primaryOccupation` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 INSERT INTO `user` VALUES
-(1,'admin','admin@infralab.local',NULL,NULL,'01/01/1990','N/A','N/A','N/A','N/A','Barcelona, Spain','Barcelona, Spain','System Administrator','DevOps Engineer','Linux Bash Docker Kubernetes','600000001','600000001','Spanish','Catalan, Spanish, English','10','$2b$11$xtX1r8mu8YS80aBSsLq/nO5/ztnCodlfrXNdZtZIg.1FlKCwBJ.By'),
-(2,'jon.snow','jon@infralab.local',NULL,NULL,'01/01/1279','Eddard Stark','Lyanna Stark','male','Single','Castle Black, The Wall','Winterfell, The North','Lord Commander','King in the North','Leadership Swordsmanship Stealth','600000002','600000002','Northerner','Common Tongue','10','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa'),
-(3,'arya.stark','arya@infralab.local',NULL,NULL,'15/03/1286','Eddard Stark','Catelyn Tully','female','Single','Braavos','Winterfell, The North','Faceless Assassin','Water Dancer','Stealth Swordsmanship Disguise','600000003','600000003','Northerner','Common Tongue, High Valyrian','5','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa'),
-(4,'tyrion.lannister','tyrion@infralab.local',NULL,NULL,'22/07/1273','Tywin Lannister','Joanna Lannister','male','Single','Casterly Rock','Kings Landing','Hand of the Queen','Master of Coin','Strategy Diplomacy Wine Tasting','600000004','600000004','Westerosi','Common Tongue, High Valyrian','8','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa'),
-(5,'daenerys.targaryen','dany@infralab.local',NULL,NULL,'10/11/1280','Aerys Targaryen','Rhaella Targaryen','female','Widowed','Dragonstone','Meereen','Queen of the Seven Kingdoms','Breaker of Chains','Dragon Riding Leadership Diplomacy','600000005','600000005','Valyrian','Common Tongue, High Valyrian, Dothraki','6','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa');
+(1,'admin','admin@infralab.local','25/09/1997','Francisco Iglesias','Clara Reguant','male','Married','Barcelona, Spain','Spanish','Catalan, Spanish, English','System Administrator','$2b$11$xtX1r8mu8YS80aBSsLq/nO5/ztnCodlfrXNdZtZIg.1FlKCwBJ.By'),
+(2,'jon.snow','jon@infralab.local','N/A','Eddard Stark','N/A','male','Single','Castle Black, The Wall','Northerner','Common Tongue','Lord Commander','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa'),
+(3,'arya.stark','arya@infralab.local','N/A','Eddard Stark','Catelyn Tully','female','Single','Braavos','Northerner','Common Tongue','Faceless Assassin','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa'),
+(4,'tyrion.lannister','tyrion@infralab.local','N/A','Tywin Lannister','Joanna Lannister','male','Divorced','Casterly Rock','Westerosi','Common Tongue, High Valyrian','Hand of the Queen','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa'),
+(5,'daenerys.targaryen','dany@infralab.local','N/A','Aerys Targaryen','Rhaella Targaryen','female','Widowed','Dragonstone','Valyrian','Common Tongue, High Valyrian, Dothraki','Queen of the Seven Kingdoms','$2b$11$tkzgoAYJALjCRVsB1EBNBui5UqZrXfWjoOHGfRns/XS1fDssdxtfa');
 
 -- ----------------------------
 -- Table: user_role
