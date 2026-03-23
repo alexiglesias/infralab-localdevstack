5e2735394247c56db1544c17f0d904c60002b808
Alex Iglesias Reguant <alex.iglesias7@gmail.com>
2026-03-23 16:47:24 +0100

    add nginx reverse proxy provisioning script

---
+++ nginx.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+
+echo ">>> Installing Nginx..."
+apt update -y
+apt install -y nginx
+
+
+echo ">>> Configuring reverse proxy..."
+cat > /etc/nginx/sites-available/infralab <<EOF
+upstream app {
+    server app01:8080;
+}
+
+
+server {
+    listen 80;
+
+
+    location / {
+        proxy_pass http://app;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+    }
+}
+EOF
+
+
+echo ">>> Enabling site..."
+rm -f /etc/nginx/sites-enabled/default
+ln -s /etc/nginx/sites-available/infralab /etc/nginx/sites-enabled/infralab
+
+
+echo ">>> Starting Nginx..."
+systemctl start nginx
+systemctl enable nginx
+systemctl restart nginx
+
+
+echo ">>> Done. Nginx is up and running."
